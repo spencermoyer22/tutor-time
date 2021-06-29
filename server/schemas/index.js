378b3f59f0cd72a3f6a3ce7d1bb3388ca66651a1
Duartem795 <76462661+Duartem795@users.noreply.github.com>
2021-06-28 20:45:10 -0700

    More server things
    Yup, just that

---
+++ server/schemas/index.js
@@ -0,0 +1,4 @@
+const typeDefs = require('./typeDefs');
+const resolvers = requires('./resolvers');
+
+module.exports = { typeDefs, resolvers };
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+//insert resolvers here
+
+const resolvers = {
+    Query: {
+        /* insert thing here */: async () => {
+            return await /* thing */.find();
+        },
+    },
+};
+
+module.exports = resolvers;
