45f058b561a619a187b10f76b70b2e5f54c8d289
Andii <andii.magyar@gmail.com>
2021-06-29 23:02:55 -0700

    nav folder

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  
+  return (
+    <div className="container" style= "background-image: url('..images/homepage.jpg')">
+      <h1>Welcome to Tutor Time!</h1>
+    </div>
+  );
+};
+
+export default Home;
