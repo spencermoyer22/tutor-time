8a519e773131bf3beda6abb625ca6e825bf4b4fb
Andii <andii.magyar@gmail.com>
2021-06-30 20:45:52 -0700

    client 'server' working

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
+import Home from './pages/Home';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Route exact path="/" component={Home} />
       </header>
     </div>
+    </Router>
   );
 }
 
